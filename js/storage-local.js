8c28bcf5491bd658ca8bfb2e249ee3f6b22a8ac8
Roni Laukkarinen <roni@laukkarinen.info>
2016-11-04 09:22:08 +0200

    Init repo - first commit

---
+++ js/storage-local.js
@@ -0,0 +1,1 @@
+"use strict";var storageLocal=function(){var r=function(r,t,a){var e={};return e[r]=JSON.stringify(t),chrome.storage.local.set(e,a),!0},t=function(r,t,a){var e={};return e[r]=JSON.stringify(t),chrome.storage.local.get(e,function(t){var e={};e[r]=JSON.parse(t[r]),a(e)}),!0};return{save:r,load:t}}();
